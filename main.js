921a74895fb35e6ffdc2b29485009b29603a0986
virginiabsch <virginiabsch@gmail.com>
2024-08-02 19:02:20 -0300

    alteração código js

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-const form = document.getElementById('to-do-list');
-const newTask = document.getElementById('new-task');
-const botaoAdicionar = document.getElementById('botao-adicionar');
-const nomeTarefa = [];
-let linhas = '';
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const nomeDaTarefa = $('input').val();
+        const novaTarefa = $(`<li>${nomeDaTarefa}</li>`)
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+        $(novaTarefa).appendTo('ul')
+        $('input').val('')
+    })
 
-    addTask();    
-    atualizaTabela();
-}) 
+    $('ul').on('click', 'li', function(e){
+        e.preventDefault();
+        $(this).toggleClass('checked')
 
-function addTask () {
-    if(nomeTarefa.includes(newTask.value)){
-        alert(`a tarefa ${newTask.value} já foi inserida!`);
-    } else { 
-    nomeTarefa.push(newTask.value);
-    linha += `<li> ${newTask.value} </li>`
-    linhas += linha;
-    }
+    })
 
-    newTask.value = '';
-}
 
-function atualizaTabela(){
-    const tabela = document.querySelector('ul');
-    tabela.innerHTML = linhas;
-}
-
-tabela.addEventListener('click', function (e) {
-    if(e.target.classList.contains('checked')){
-        e.target.classList.remove('ckecked');
-    }else{
-        e.target.classList.add('checked');
-    }
-});
+})
 
 
